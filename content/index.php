3d3a6c1642f783702ecced3c7918628ee2bfddce
raph <the.small.swamp@gmail.com>
2022-04-19 15:00:58 -0400

    better hello world and ignores

--- content/index.php
+++ content/index.php
@@ -1,6 +1,17 @@
 <!doctype html>
-<body>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Hello world!</title>
+	</head>
+	<body>
 <?php 
 echo 'hello world!';
 ?>
+	</body>
+</html>
+
+<!doctype html>
+<body>
+
 </body>
